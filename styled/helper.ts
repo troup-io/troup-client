d35477ae77f12a58cf60a0b92b63c6e244dcff3e
Samrith Shankar <samrith.298@gmail.com>
2020-05-19 23:37:52 +0200

    Added more helper methods to styled

--- styled/helper.ts
+++ styled/helper.ts
@@ -1,3 +1,4 @@
+import { css } from 'styled-components';
 import {
     padding as _padding,
     margin as _margin,
@@ -5,6 +6,7 @@ import {
     lighten as _lighten,
     transparentize as _transparentize,
     opacify as _opacify,
+    rgba as _rgba,
 } from 'polished';
 
 import { Theme, ColorsType, VariablesType } from './theme';
@@ -13,39 +15,70 @@ interface Props {
     theme: Theme;
 }
 
-export const color = (key: ColorsType): any => (props: Props): string => props.theme.colors[key];
+export const color = (key: ColorsType): any => (props: Props): string => props.theme.brand[key];
 
 export const variable = (key: VariablesType): any => (props: Props): string | number =>
     props.theme.variables[key];
 
-export const padding = (top = 0, right = undefined, bottom = undefined, left = undefined) => (
-    props: Props
-): any =>
+export const borderRadius = (
+    topLeft = 1,
+    topRight = undefined,
+    bottomRight = undefined,
+    bottomLeft = undefined
+) => ({
+    theme: {
+        variables: { borderRadius },
+    },
+}: Props): any => css`
+    border-radius: ${borderRadius * topLeft}px ${borderRadius * (topRight ?? topLeft)}px
+        ${borderRadius * (bottomRight ?? topLeft)}px
+        ${borderRadius * (bottomLeft ?? topRight ?? topLeft)}px;
+`;
+
+export const border = (
+    size = 1,
+    color?: ColorsType,
+    side?: 'top' | 'left' | 'bottom' | 'right'
+) => ({ theme: { brand, variables } }: Props): any => css`
+    ${side ? `border-${side}` : 'border'}: ${variables.borderSize * size}px solid ${
+    brand[color ?? 'dim']
+};
+`;
+
+export const padding = (top = 1, right = undefined, bottom = undefined, left = undefined) => ({
+    theme: {
+        variables: { padding },
+    },
+}: Props): any =>
     _padding(
-        (props.theme.variables.padding as number) * top,
-        (props.theme.variables.padding as number) * (right ?? top),
-        (props.theme.variables.padding as number) * (bottom ?? top),
-        (props.theme.variables.padding as number) * (left ?? right ?? top)
+        padding * top,
+        padding * (right ?? top),
+        padding * (bottom ?? top),
+        padding * (left ?? right ?? top)
     );
 
-export const margin = (top = 0, right = 0, bottom = undefined, left = undefined) => (
-    props: Props
-): any =>
-    _margin(
-        (props.theme.variables.margin as number) * top,
-        (props.theme.variables.margin as number) * right,
-        (props.theme.variables.margin as number) * (bottom ?? top),
-        (props.theme.variables.margin as number) * (left ?? left)
-    );
+export const margin = (top = 0, right = 0, bottom = undefined, left = undefined) => ({
+    theme: {
+        variables: { margin },
+    },
+}: Props): any =>
+    _margin(margin * top, margin * right, margin * (bottom ?? top), margin * (left ?? left));
 
 export const darken = (key: ColorsType, value: number) => (props: Props): any =>
-    _darken(value, props.theme.colors[key]);
+    _darken(value, props.theme.brand[key]);
 
 export const lighten = (key: ColorsType, value: number) => (props: Props): any =>
-    _lighten(value, props.theme.colors[key]);
+    _lighten(value, props.theme.brand[key]);
 
 export const transparentize = (key: ColorsType, value: number) => (props: Props): any =>
-    _transparentize(value, props.theme.colors[key]);
+    _transparentize(value, props.theme.brand[key]);
 
-export const opacity = (key: ColorsType, value: number) => (props: Props): any =>
-    _opacify(value, props.theme.colors[key]);
+export const opacify = (key: ColorsType, value: number) => (props: Props): any =>
+    _opacify(value, props.theme.brand[key]);
+
+export const rgba = (key: ColorsType, value: number) => (props: Props): any =>
+    _rgba(props.theme.brand[key], value);
+
+export const boxShadow = (value?: number, color?: ColorsType) => (props: Props): any => css`
+    box-shadow: 0 2px 5px 1px ${rgba(color ?? 'defaultBorder', value ?? 0.2)};
+`;
