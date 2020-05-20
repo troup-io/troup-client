9951b8e5e447d98151808d741c944d4e79db4e80
Samrith Shankar <samrith.298@gmail.com>
2020-05-20 03:00:46 +0200

    Added dummy menu component for top-right corner

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,20 +1,17 @@
 import React from 'react';
-import Link from 'next/link';
 import { Flex, Text } from '@primer/components';
+
+import { Link } from '@atoms/Link';
 import { ButtonPrimary } from '@atoms/Button';
 
 export const Menu: React.FC = () => {
     return (
         <Flex alignItems="center" justifyContent="flex-end">
             <Text mr={5}>
-                <Link href="#">
-                    <a>Features</a>
-                </Link>
+                <Link href="#">Features</Link>
             </Text>
             <Text mr={5}>
-                <Link href="/login">
-                    <a>Login</a>
-                </Link>
+                <Link href="/login">Login</Link>
             </Text>
             <Link href="/signup">
                 <ButtonPrimary>Signup</ButtonPrimary>
