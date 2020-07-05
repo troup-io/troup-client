39ce24e1394279b39f76c7179766ccf7e50aa7f0
Samrith Shankar <samrith.298@gmail.com>
2020-07-05 23:48:38 +0200

    Add AvatarText

--- system/atoms/Avatar.tsx
+++ system/atoms/Avatar.tsx
@@ -7,3 +7,18 @@ export const Avatar = styled(_Avatar)`
     background: ${color('dimmer')};
     transition: all 0.25s ease;
 `;
+
+interface AvatarTextProps {
+    size?: number;
+}
+
+export const AvatarText = styled.div<AvatarTextProps>`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: ${(props): number => props.size}px;
+    height: ${(props): number => props.size}px;
+    color: ${color('white')};
+    background: ${color('primaryDark')};
+    ${borderRadius(1)};
+`;
