ccbb55a25a153286be8379866108fcd9c7eb3979
Samrith Shankar <samrith.298@gmail.com>
2020-07-05 23:49:15 +0200

    Update Loading positioning

--- system/molecules/Loading.tsx
+++ system/molecules/Loading.tsx
@@ -9,22 +9,31 @@ import { rgba } from '@styled/helper';
 export interface LoadingProps {
     opacify?: boolean;
     size?: number;
+    fixed?: boolean;
 }
 
 const LoadingWrapper = styled(Flex)<LoadingProps>`
-    position: absolute;
+    position: ${(props) => (props.fixed ? 'fixed' : 'absolute')};
     top: 0;
     right: 0;
     bottom: 0;
     left: 0;
+    width: 100%;
+    height: 100%;
     background: ${(props) => rgba('light', props.opacify ? 1 : 0.75)};
     z-index: 999;
 `;
 
-export const Loading: React.FC<LoadingProps> = ({ opacify, size = 75 }) => {
+export const Loading: React.FC<LoadingProps> = ({ size, ...rest }) => {
     return (
-        <LoadingWrapper alignItems="center" justifyContent="center">
+        <LoadingWrapper alignItems="center" justifyContent="center" {...rest}>
             <Spinner size={size} />
         </LoadingWrapper>
     );
+};
+
+Loading.defaultProps = {
+    opacify: false,
+    size: 75,
+    fixed: false,
 };
