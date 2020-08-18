92458c7eb186414e9de5b04f991d70e7eec10f1e
Samrith Shankar <samrith.298@gmail.com>
2020-08-18 23:22:47 +0200

    Updated Team outlet

--- src/components/Team/index.tsx
+++ src/components/Team/index.tsx
@@ -1,30 +1,18 @@
 import React from 'react';
 import { Outlet, Navigate, useOutlet } from 'react-router-dom';
 
-import { GetProjects_projects } from 'server-types/GetProjects';
-import { GET_PROJECTS } from 'data/queries';
-
-import { useQuery } from 'hooks/useQuery';
-import { useCrux } from 'hooks/useCrux';
+import { Content } from 'system/atoms/Content';
 
 export const Team: React.FC = () => {
-    const { team } = useCrux();
     const outlet = useOutlet();
-    const { loading, error, data } = useQuery<GetProjects_projects[]>(GET_PROJECTS, {
-        skip: !!outlet,
-    });
-
-    if (loading) {
-        return <>Loading..</>;
-    }
-
-    if (error) {
-        return <>{error.message}</>;
-    }
 
     if (!outlet) {
-        return <Navigate to={`/${team?.name}/projects/${data?.[0].sequence}`} />;
+        return <Navigate to="projects" replace />;
     }
 
-    return <Outlet />;
+    return (
+        <Content p={3}>
+            <Outlet />
+        </Content>
+    );
 };
